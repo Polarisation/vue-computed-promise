75b24fd0748bd81f56e485bdf939ad93914891dd
Justin Emery <justin.emery@gmail.com>
2017-03-20 14:26:37 +0000

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # vue-computed-promise
-Allow promises to be returned for Vue.js computed properties
+Plugin for Vue.js, allows promises to be returned for computed properties
+
+
+[![NPM](https://nodei.co/npm/vue-computed-promise.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/vue-computed-promise/)
+
+
+## Install
+
+```
+$ npm install vue-computed-promise
+```
+
+## Usage
+
+Add the plugin:
+
+```
+const VueComputedPromise = require('vue-computed-promise'); // alternatively use script tag
+Vue.use(VueComputedPromise);
+```
+
+Now you can return a Promise from a computed property:
+
+```
+var vue = new Vue({
+  el: "#app",
+  data: {
+    one: 1,
+    two: 2
+  },
+  computed: {
+    oneplustwo: function() {
+      var _one = this.$data.one;
+      var _two = this.$data.two;
+      return new Promise(function(resolve, reject) {
+        setTimeout(0, function() {
+          resolve(_one + _two);
+        });
+      });
+    }
+  }
+})
+```
+
+Until the Promise resolves, the value of `null` is returned by the computed property.
+
+In the example above `oneplustwo` is never re-computed. The Promise will only be called once (or never if the property is not used in the template).
+
+To return a Promise which may be re-computed when reactive dependencies change, you will need to return a function which in turn returns a Promise:
+
+```
+var vue = new Vue({
+  el: "#app",
+  data: {
+    a: 1,
+    b: 2,
+    calculating: false,
+    count: 0
+  },
+  computed: {
+    result: function() {
+      // these properties are reactive dependencies
+      var _a = Number(this.a);
+      var _b = Number(this.b);
+
+      return () => {
+        // properties only accessed within this function are not reactive dependencies
+        var data = this.$data;
+        data.calculating = true;
+
+        return new Promise(function(resolve, reject) {
+          setTimeout(function() {
+            resolve(_a + _b);
+            data.calculating = false;
+            data.count++;
+          }, 1000);
+        });
+      };
+    }
+  }
+})
+```
+
+As the example shows, you also have control over which properties become dependencies: only properties accessed outside of the returned function will become dependencies.
+
+## Contributions
+
+I've only used this for a limited use case - but I'm happy to take pull requests to improve the plugin.
---
+++ vue-computed-promise.js
@@ -0,0 +1,76 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.VueComputedPromise = factory());
+}(this, (function () { 'use strict';
+
+var VueComputedPromise = {
+	install: function(Vue, options) {
+		Vue.mixin({
+			beforeCreate: function() {
+				let oldData = this.$options.data;
+				let oldComputed = this.$options.computed || {};
+
+				// cerate placeholder data properties
+				this.$options.data = function() {
+          let newData = (
+              (typeof oldData === 'function')
+                ? oldData.call(this)
+                : oldData
+             ) || {};
+          Object.keys(oldComputed).forEach(key => {
+            newData[key + "VueComputedPromisePlaceholder"] = null;
+          })
+          return newData;
+				};
+
+        // wrap computed properties
+				this.$options.computed = {};
+				Object.keys(oldComputed).forEach(key => {
+					let promiseInitiated = false;
+					this.$options.computed[key] = function() {
+						let data = this.$data;
+						let result = oldComputed[key].call(this);
+
+            if(result && typeof result === "function") {
+							if(!promiseInitiated) {
+								promiseInitiated = true;
+	              var promise = result();
+	              if(promise && promise.then && promise.catch) { // looks like a Promise
+	                  // var oldValue = data[key + "VueComputedPromisePlaceholder"];
+	                  promise.then(r => {
+	                    data[key + "VueComputedPromisePlaceholder"] = r;
+	                  });
+	                  // return placeholder until Promise result is ready
+	                  return data[key + "VueComputedPromisePlaceholder"];
+	              } else { // not actually a promise
+	                throw new Error("VueComputedPromise: "+key+" function returned must return a Promise");
+	              }
+							} else {
+								promiseInitiated = false;
+								return data[key + "VueComputedPromisePlaceholder"];
+							}
+            }
+
+            if(result && result.then && result.catch) { // looks like a Promise
+							result.then(r => {
+								oldComputed[key] = () => r;  // ensure promise will not be called in loop
+
+							  // overwrite placeholder with final result of Promise
+								data[key + "VueComputedPromisePlaceholder"] = r;
+							})
+							// return placeholder until Promise result is ready
+							return data[key + "VueComputedPromisePlaceholder"];
+						}
+
+            else // standard vue behavior
+							return result;
+					}
+				});
+			}
+		})
+	}
+};
+return VueComputedPromise;
+
+})));
